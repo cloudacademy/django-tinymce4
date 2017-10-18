3e1174d0a241c438a582883ec0c0b4998e7aaf96
lzac <luca.zacchetti@cloudacademy.com>
2017-10-18 12:54:47 +0200

    PEP8 in setup.py

--- setup.py
+++ setup.py
@@ -4,24 +4,25 @@ import metadata
 
 app_name = metadata.name
 version = metadata.version
+
+
 def read(filename):
     with open(filename) as fp:
         return fp.read()
 long_description = read('README.md')
 
 setup(
-    name = "django-%s" % app_name,
-    version = version,
-    packages = find_packages(),
-    include_package_data = True,
-    author = "Daniele Bernardini",
-    author_email = "db@intranetstandard.com",
-    description = "A Django application that contains a widget to render a" \
-            " form field as a TinyMCE 4 editor.",
-    long_description = long_description,
-    license = "MIT License",
-    keywords = "django widget tinymce",
-    classifiers = [
+    name="django-%s" % app_name,
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    author="Daniele Bernardini",
+    author_email="db@intranetstandard.com",
+    description="A Django application that contains a widget to render a form field as a TinyMCE 4 editor.",
+    long_description=long_description,
+    license="MIT License",
+    keywords="django widget tinymce",
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -32,6 +33,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    platforms = ['any'],
-    url = "https://github.com/dani0805/django-tinymce4",
+    platforms=['any'],
+    url="https://github.com/cloudacademy/django-tinymce4",
 )
